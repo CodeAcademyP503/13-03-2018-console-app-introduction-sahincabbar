0c81e5a900761f096b064caf682a82542d9309a7
sahincabbar <36867588+sahincabbar@users.noreply.github.com>
2018-03-15 21:37:40 +0400

    tasklar

---
+++ ConsoleApp26/Program.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp26
+{
+    class Program
+    {
+        static void Main(string[] args) {
+
+
+
+
+            /*7 Bankomatdan pul cekmeye gedmisem. Ekrandan cekmek istediyim pul migdarini daxil edirem.Bankomat mene mumkun olan en boyuy eskinazlari vermelidir. meselen: 754 azn pul cekmek isteyiremse 7 eded yuzluk 1 eded 50 lik ve 4 eded birlik eskinaz vermelidir.Bunu reallasdiracaq proqram yazin.
+            List<int> eded1 = new List<int> { 100, 50, 20, 10, 5, 1 };
+            List<string> sozler = new List<string> { "yuzduk", "ellilik", "iyirmilik", "onluq", "beslik", "birlik" };
+            var eded = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < eded1.Count; i++)
+            {
+                if (eded >= eded1[i])
+                {
+                    var reqem = eded / eded1[i];
+                    eded = eded - reqem * eded1[i];
+                    Console.WriteLine(reqem + " - " + sozler[i]);
+                }
+
+            }
+
+            //  6 Ekrandan daxil edilen ifade icerisinde "tenbel" sozu varsa eyni ifadeni sadece tenbel sozunun evezine "loser" olaraq deyisdirerek yazdirmasini temin eden program yazin
+            //Console.WriteLine("bir ifade daxil edin");
+            //string text = Console.ReadLine();
+            //Console.Write(" after replacing  : ");
+            //Console.WriteLine(text.Replace("tenbel", "loser"));
+
+
+            //5.Ekrandan daxil olunan ifadede bu sozlerden her hansi biri kecirse ekranda "Siz losersiniz " deye error vermesini isteyirem.
+
+            //List<string> keywords = new List<string>() { "Tenbel", "Kod yazmayan", "Yola Veren", "Esneyen", "Bekar" };
+
+            //Console.WriteLine("her hansi bir cumle yazin");
+
+            //string txt = Console.ReadLine();
+
+
+
+            //string[] splitedWords = txt.Split(',',' ');
+
+
+
+            //bool checking = false;
+
+            //foreach (string item in splitedWords)
+            //{
+
+
+            //    for (int i = 0; i < keywords.Count; i++)
+            //    {
+            //        for (int z = 0; z < splitedWords.Length; z++)
+            //        {
+            //            if (splitedWords[z] == keywords[i])
+            //            {
+
+            //                checking = true;
+            //                break;
+            //            }
+            //        }
+
+
+            //    }
+
+
+            //}
+
+            //if (checking == true)
+            //{
+            //    Console.WriteLine("siz losersiniz ");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("siz loser deyilsiniz ");
+            //}
+
+
+
+            //  4 Ekrandan daxil olunan cumlenin sozlerini tek tek ekrana cixaran program yazin
+
+            //Console.WriteLine("ekrana bir cumle yazin");
+            //string txt = Console.ReadLine();
+            //var soz = txt.Split('a');
+            //Console.WriteLine("sozler : ");
+            //foreach (var item in txt)
+            //{
+
+            //    Console.Write(item);
+            //}
+            //Console.ReadKey();
+
+            //3 Ekrandan daxil olunan ifadenin daxilinde sait ve samitleri ayri ayri ekrana yazdiran program yazin
+
+
+            //    Console.Write("ifadeni girin: ");
+            //string ifade = Console.ReadLine();
+            //string sait = "", samit = "";
+            //ifade = ifade.ToLower();
+            //int sayi = ifade.Length;
+            //for (int i = 0; i <= sayi - 1; i++)
+            //{
+            //    char herf = Convert.ToChar(ifade.Substring(i, 1));
+            //    if (herf == 'a' || herf == 'i' || herf == 'o' || herf == 'u' || herf == 'e' || )
+            //    {
+            //        sait += herf + "-";
+            //    }
+            //    else
+            //    {
+            //        samit += herf + "-";
+            //    }
+            //}
+            //Console.WriteLine("saitler: " + sait);
+            //Console.WriteLine("samitler: " + samit);
+            //Console.Read();
+
+
+            // 2 Ekrandan girilen ededin reqemleri cemini ekrana yazdiran program yazin
+            //Console.WriteLine("reqem daxil edin : ");
+            //int reqem = Convert.ToInt32(Console.ReadLine());
+
+            //int total = 0;
+            //do
+            //{
+            //    total += reqem % 10;
+            //    reqem /= 10;
+
+            //} while (reqem > 0);
+            //Console.WriteLine("total " + " = "+ total);
+
+
+
+
+
+            //  1 Ekrandan girilen ededin nece reqemli oldugunu ekrana yazdiran proqram yazin
+
+
+
+
+
+            //Console.WriteLine("Bir eded girin");
+            //int eded = Convert.ToInt32(Console.ReadLine());
+            //int num = 1;
+
+            //string str = "daxil elediyiniz eded --  ";
+            //string txt = " reqemlidir ";
+            //while (eded > 9)
+            //{
+            //    eded /= 10;
+            //    num++;
+            //}
+            //Console.WriteLine(str + num + txt);*/
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
